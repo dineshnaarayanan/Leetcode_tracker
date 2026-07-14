902ba104fda8269b546f7056e236a48a4ad30934
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-14 13:58:58 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 1:58:58 PM
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        int j = 0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=nums[j]){
+                j++;
+                nums[j]=nums[i];
+            }
+         }
+       return j+1;
+        }
+}
