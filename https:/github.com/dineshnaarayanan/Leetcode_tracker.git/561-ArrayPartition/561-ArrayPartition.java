e0c8cfe7bd1cfcf252beac3da1e72456f4c86e11
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-14 13:58:02 +0530

    Create 561-ArrayPartition

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/561-ArrayPartition/561-ArrayPartition.java
@@ -0,0 +1,11 @@
+// Last updated: 7/14/2026, 1:58:02 PM
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max=0;
+        for (int i=0;i<nums.length;i+=2){
+            max+=nums[i];
+        }
+        return max;
+    }
+}
