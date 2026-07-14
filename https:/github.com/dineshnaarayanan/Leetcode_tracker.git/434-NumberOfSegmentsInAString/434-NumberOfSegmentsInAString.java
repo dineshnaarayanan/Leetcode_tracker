c5fae23f49cbdc628e801a357348b0df94f49ac6
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-14 13:58:05 +0530

    Create 434-NumberOfSegmentsInAString

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/434-NumberOfSegmentsInAString/434-NumberOfSegmentsInAString.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 1:58:05 PM
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
