ca2dddb2c1dc460abb347e6b711008d835b0b1fd
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-14 13:58:31 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.java
@@ -0,0 +1,27 @@
+// Last updated: 7/14/2026, 1:58:30 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+}
