9c4d3ce955b87be47792e85448e540b4c9fd4843
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-14 13:57:41 +0530

    Create 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/2542-AverageValueOfEvenNumbersThatAreDivisibleByThree/2542-AverageValueOfEvenNumbersThatAreDivisibleByThree.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 1:57:41 PM
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for(int num:nums)
+            if (num%6==0) {
+                sum += num;
+                count++;
+            }
+        return count==0?0:Math.round(sum/count);
+    }
+}
