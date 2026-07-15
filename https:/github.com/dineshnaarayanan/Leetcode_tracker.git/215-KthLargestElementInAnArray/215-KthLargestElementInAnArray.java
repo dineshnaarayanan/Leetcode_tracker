2590cac73e75758792a09fc043be9eb2ce257f24
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-15 12:16:05 +0530

    Create 215-KthLargestElementInAnArray

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.java
@@ -0,0 +1,14 @@
+// Last updated: 7/15/2026, 12:16:04 PM
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            count++;
+            if (count == k) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
