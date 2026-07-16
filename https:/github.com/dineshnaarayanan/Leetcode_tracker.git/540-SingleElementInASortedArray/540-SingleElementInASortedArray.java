8f531564576b708809e3f99b7bc4ce02df046204
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-16 10:11:23 +0530

    Create 540-SingleElementInASortedArray

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/540-SingleElementInASortedArray/540-SingleElementInASortedArray.java
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 10:11:22 AM
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int num:nums){
+            if(map.get(num)==1){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
