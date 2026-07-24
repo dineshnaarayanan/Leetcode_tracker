e6911e77f4462436107145cdde045d1903536ca3
Dinesh Naarayanan K <145882716+dineshnaarayanan@users.noreply.github.com>
2026-07-24 18:26:41 +0530

    Create 27-RemoveElement

---
+++ https:/github.com/dineshnaarayanan/Leetcode_tracker.git/27-RemoveElement/version/java/27-RemoveElement_20260724_182639.java
@@ -0,0 +1,14 @@
+// Last updated: 7/24/2026, 6:26:39 PM
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        int index=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(nums[i]!=val){
+6                nums[index]=nums[i];
+7                index++;
+8
+9            }
+10        }
+11        return index;
+12    }
+13}
